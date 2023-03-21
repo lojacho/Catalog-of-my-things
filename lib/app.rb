3d24b5f4bcbfd27c1598f75d3f62c34414f5ad0b
Victor <victorgmassimi.microverse@gmail.com>
2023-03-21 09:37:00 -0600

    fix rubocop linters

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,22 @@
+ACTIONS = {
+  0 => :exit,
+  1 => :list_books,
+  2 => :list_music_albums,
+  3 => :list_games,
+  4 => :list_genres,
+  5 => :list_labels,
+  6 => :list_authors,
+  7 => :add_book,
+  8 => :add_music_album,
+  9 => :add_game
+}.freeze
+
 class App
   def initialize
     @items = {}
   end
-  def run
-    loop do 
+
+  def show_menu
     puts '0) Exit'
     puts '1) List all books'
     puts '2) List all music albums'
@@ -15,35 +28,23 @@ class App
     puts '8) Add a music album'
     puts '9) Add a game'
     puts 'Choose an option by number:'
-    option = gets.chomp.to_i
-    action (option)
-    while not option.zero?
   end
 
-  def action(num)
-   case num 
-   when 1
-    false
-   when 2 
-     false
-   when 3 
-     false
-   when 4 
-     false
-   when 5 
-     false
-   when 6
-     false
-   when 7 
-     false
-   when 8 
-     false
-   when 9
-     false
-   when 0
-    puts 'thanks for use this app'
-   else
-    puts ' this option is invalid'    
+  def run
+    option = -1
+    until option.zero?
+      show_menu
+      option = gets.chomp.to_i
+      if option.between?(0, 9)
+        action = ACTIONS[option]
+        send(action)
+      else
+        puts 'Invalid option'
+      end
+    end
   end
+
+  def exit
+    puts 'Thanks for using this app.'
   end
 end
