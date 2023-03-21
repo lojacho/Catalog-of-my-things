31e32661937988cf9b4602d6903fd4e9754dce8c
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-03-21 16:27:51 -0500

    add method can_be_archived

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,4 @@
+require 'date'
 require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
@@ -7,4 +8,11 @@ class Game < Item
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
+
+  def can_be_archived?
+    return true if years_between_dates > 2
+
+    false
+  end
 end
+
