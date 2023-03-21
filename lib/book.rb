50f36c1c561b7c3d98968a5c6a69371e264000c2
Victor <victorgmassimi.microverse@gmail.com>
2023-03-21 09:54:33 -0600

    Add book class

---
+++ lib/book.rb
@@ -0,0 +1,21 @@
+require_relative './item'
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  # Needs a hash as argument for super
+  def initialize(publisher:, cover:, **args)
+    super(genre: args[:genre] || nil,
+          author: args[:author] || nil,
+          source: args[:source] || nil,
+          label: args[:label] || nil,
+          publish_date: args[:publish_date] || nil)
+    @publisher = publisher
+    @cover_state = cover
+  end
+
+  private
+
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
+end
