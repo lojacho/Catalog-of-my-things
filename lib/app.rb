3e3f0a9ef42273d11c444004d937fba2bed8108e
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-03-21 10:16:15 -0500

    Add run loop

--- lib/app.rb
+++ lib/app.rb
@@ -2,8 +2,8 @@ class App
   def initialize
     @items = {}
   end
-
   def run
+    loop do 
     puts '0) Exit'
     puts '1) List all books'
     puts '2) List all music albums'
@@ -15,6 +15,35 @@ class App
     puts '8) Add a music album'
     puts '9) Add a game'
     puts 'Choose an option by number:'
-    _option = gets
+    option = gets.chomp.to_i
+    action (option)
+    while not option.zero?
+  end
+
+  def action(num)
+   case num 
+   when 1
+    false
+   when 2 
+     false
+   when 3 
+     false
+   when 4 
+     false
+   when 5 
+     false
+   when 6
+     false
+   when 7 
+     false
+   when 8 
+     false
+   when 9
+     false
+   when 0
+    puts 'thanks for use this app'
+   else
+    puts ' this option is invalid'    
+  end
   end
 end
