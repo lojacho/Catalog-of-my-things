70b2019c6e2ef80e5350582f01a520bed837a729
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-03-22 21:55:29 -0500

    Add another test required in the las review

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/author'
+require_relative '../lib/item'
 
 describe 'Test Author class' do
   author = Author.new(first_name: 'Osvaldo', last_name: 'Barrios')
@@ -13,5 +14,11 @@ describe 'Test Author class' do
 
   it "author last name should be 'Osvaldo'" do
     expect(author.last_name).to eql 'Barrios'
+  end
+
+  it '#add_item adds self to item as well' do
+    item = Item.new(publish_date: '2010-10-10')
+    author.add_item(item)
+    expect(author.items.include?(item)).to be true
   end
 end
